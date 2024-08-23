3be478fe23d84c36c02b8f1def369b56d6770740
Anya Shafer <anya.shafer@gmail.com>
2024-08-22 21:55:15 -0400

    updated style

--- src/components/roadmap/Roadmap.js
+++ src/components/roadmap/Roadmap.js
@@ -2,53 +2,54 @@ import React from 'react';
 import '../../index.css';
 import logo from '../../assets/bbld/top_page_logo.png'; // Adjust the path if necessary
 import { Link } from 'react-router-dom';
+
 function Roadmap() {
   return (
     <div className="main-container">
       <img src={logo} alt="BBLD Logo" className="logo" />
       <h2>Roadmap</h2>
 
-      <div className="roadmap-phase">
-        <h3>PHASE 1: 100 Holders</h3>
-        <ul>
-          <li>Deploy on Uniswap</li>
-          <li>Release 1 of 100 NFT for OGs</li>
-          <li>Create BBLD army of trolls posting content for Early BBLD tokens</li>
-          <li>Opportunity to trade BBLD for merch</li>
-          <li>Reach 1 Million Market Cap</li>
-          <li>Collaborate with smaller artists who create Drake BBL diss music</li>
-        </ul>
+      <div className="roadmap-container">
+        <div className="roadmap-phase">
+          <h3>PHASE 1: 100 Holders</h3>
+          <ul>
+            <li>Deploy on Uniswap</li>
+            <li>Release 1 of 100 NFT for OGs</li>
+            <li>Create BBLD army of trolls posting content for Early BBLD tokens</li>
+            <li>Opportunity to trade BBLD for merch</li>
+            <li>Reach 1 Million Market Cap</li>
+            <li>Collaborate with smaller artists who create Drake BBL diss music</li>
+          </ul>
+        </div>
+
+        <div className="roadmap-phase">
+          <h3>PHASE 2: 250 Holders</h3>
+          <ul>
+            <li>Release 1-10,000 NFTs</li>
+            <li>Reach 10 Million Market Cap</li>
+            <li>Collaborate with medium-sized artists for Drake BBL diss music or ART</li>
+            <li>Add web3 login for site features (must own at least 10 BBLD to access)</li>
+            <li>Introduce wrapped pairs on other Decentralized Exchanges besides Uniswap</li>
+            <li>Implement brand ambassadors to help make money and initiate a buy-back program</li>
+          </ul>
+        </div>
+
+        <div className="roadmap-phase">
+          <h3>PHASE 3: 1000+ Holders</h3>
+          <ul>
+            <li>Reach 100 Million Market Cap</li>
+            <li>Listing on Centralized Exchanges</li>
+            <li>Engage large celebrity influencers</li>
+            <li>Develop Drake BBL Maze Runner game</li>
+          </ul>
+        </div>
       </div>
 
-      <div className="roadmap-phase">
-        <h3>PHASE 2: 250 Holders</h3>
-        <ul>
-          <li>Release 1-10,000 NFTs</li>
-          <li>Reach 10 Million Market Cap</li>
-          <li>Collaborate with medium-sized artists for Drake BBL diss music or ART</li>
-          <li>Add web3 login for site features (must own at least 10 BBLD to access)</li>
-          <li>Introduce wrapped pairs on other Decentralized Exchanges besides Uniswap</li>
-          <li>Implement brand ambassadors to help make money and initiate a buy-back program</li>
-        </ul>
-      </div>
-
-      <div className="roadmap-phase">
-        <h3>PHASE 3: 1000+ Holders</h3>
-        <ul>
-          <li>Reach 100 Million Market Cap</li>
-          <li>Listing on Centralized Exchanges</li>
-          <li>Engage large celebrity influencers</li>
-          <li>Develop Drake BBL Maze Runner game</li>
-        </ul>
-      </div>
       <Link to="/">
         <button className="button">Back</button>
       </Link>
     </div>
   );
 }
-
-
-
 
 export default Roadmap;
--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import '../../index.css';
 import logo from '../../assets/bbld/top_page_logo.png'; // Adjust the path if necessary
+import kikil0 from '../../assets/kikil0.png'; // Image for Team Member 1
+import sudoakula from '../../assets/sudoakula.png'; // Image for Team Member 2
 import { Link } from 'react-router-dom';
+
 function Team() {
   return (
     <div className="main-container">
-      <img src={logo} alt="BBLD Logo" className="logo" />
-      <h2>Team</h2>
+      <h2>Our Team</h2>
+      <div className="team-members">
+        <div className="team-member">
+          <img src={kikil0} alt="kikil0" className="team-image" />
+          <h3>kikil0</h3>
+          <p className="role"><b>Role:</b> Marketer</p>
+          <p className="description">
+            Strategic marketing leader with an MBA in Management and a strong background in Web3 projects, as well as front-end and back-end development. Expertise in both marketing and technology enables them to bridge the gap between creative and technical aspects, delivering results that align with business goals. Passionate about leveraging emerging technologies, dedicated to pushing the boundaries of what's possible in the digital landscape.
+          </p>
+        </div>
+        <div className="team-member">
+          <img src={sudoakula} alt="sudoakula" className="team-image" />
+          <h3>sudoakula</h3>
+          <p className="role"><b>Role:</b> Developer</p>
+          <p className="description">
+            Web3 and Blockchain Specialist, focusing on Decentralized Finance (DeFi) solutions. Successfully deployed a variety of smart contracts on Ethereum, including NFTs, ERC20, ERC721(A), and DAOs. Certified and Highly Educated, holding a master's degree in cybersecurity, CISSP certification, and TS/SCI clearance. Currently engaged in PhD research on asynchronous consensus protocols for blockchain systems.
+          </p>
+        </div>
+      </div>
 
-      <p>
-        This will be were the team will tell more about ourselves and who we are and why we are doing this...
-      </p>
       <Link to="/">
-          <button className="button">Back</button>
+        <button className="button">Back</button>
       </Link>
     </div>
   );
